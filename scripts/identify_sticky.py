ffb0e2d975f72168985b0525bdb382e01a095215
haydenm2 <haydenmmorgan@gmail.com>
2018-12-08 11:21:25 -0700

    Fully functional image and ir sensor subscribing and publication.  A little touch and needs a distinct background from the yellow sticky...I used a purple foam board and traced the sticky note in black to emphasize the yellow edge.

--- scripts/identify_sticky.py
+++ scripts/identify_sticky.py
@@ -19,7 +19,7 @@ from std_msgs.msg import String
 from baxter_follow.msg import CamArray
 #import baxter_interface
 import cv2
-import cv_bridge
+from cv_bridge import CvBridge, CvBridgeError
 
 	#SUDO CODE:
 
@@ -39,15 +39,22 @@ def ir_callback(message):
     global ir
     # Convert image from a ROS image message to a CV image
     ir = message.range
+
     # Desirable range is probably around 0.2 meters
-    rospy.loginfo("receiving ir %s", ir)
+#    rospy.loginfo("receiving ir %s", ir)
 
 def camera_callback(message):
     global img
     # Convert image from a ROS image message to a CV image
-    bridge = cv_bridge.CvBridge()
-    img = bridge.imgmsg_to_cv2(message, "bgr8")
-    rospy.loginfo("receiving images")
+    try:
+        bridge = CvBridge()
+        img = bridge.imgmsg_to_cv2(message, "bgr8")
+        #rospy.sleep(0.001)
+#        rospy.loginfo("receiving images")
+    except CvBridgeError, e:
+        print(e)
+#        rospy.loginfo("receiving images error")
+    
 
 def main():
     rospy.init_node('identify_sticky') 	#start identify_sticky node
@@ -56,23 +63,30 @@ def main():
     # create subscriber to the required camera
     camera_str = "/cameras/right_hand_camera/image"
     ir_str = "/robot/range/right_hand_range/state"
-#    camsub = rospy.Subscriber(camera_str, Image, camera_callback)	#init camera subscriber
-    irsub = rospy.Subscriber(ir_str, Range, ir_callback)	#init ir rangefinder subscriber
+    camsub = rospy.Subscriber(camera_str, Image, camera_callback, queue_size=1)	#init camera subscriber
+    irsub = rospy.Subscriber(ir_str, Range, ir_callback, queue_size=1)	#init ir rangefinder subscriber
     cam_pub = rospy.Publisher('cam_data', CamArray, queue_size=1)
     global img
     global ir
-
+    ir = 1
+    rospy.sleep(0.1)
     while not rospy.is_shutdown():
-        img = cv2.imread("/home/radlab/baxter_hayden/src/baxter_follow/other/sticky.png")  #load sample image for debugging
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)  #convert RGB to HSV image
+        rospy.loginfo("Identify Sticky Running")
 
-        lower_yel = np.array([23,150,50]) #yellow lower bound definition
-        upper_yel = np.array([35,255,255]) #yellow upper bound definition
+#        img = cv2.imread("/home/radlab/baxter_hayden/src/baxter_follow/other/sticky.png")  #load sample image for debugging
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)  #convert RGB to HSV image
+        lower_yel = np.array([23,50,50]) #yellow lower bound definition
+        upper_yel = np.array([50,255,255]) #yellow upper bound definition
+
+        lower = np.array([0,0,0]) #yellow lower bound definition
+        upper = np.array([255,1,255]) #yellow upper bound definition
 
         mask = cv2.inRange(hsv, lower_yel, upper_yel)
         res = cv2.bitwise_and(img,img, mask= mask)
+        gray_image = cv2.cvtColor(res, cv2.COLOR_RGB2GRAY)
 
-        if cv2.countNonZero(mask) <= 10:
+        
+        if cv2.countNonZero(mask) <= 20:
 #            print "No Sticky Note Detected"    #for debugging
             sticky = 0
             height, width = img.shape[:2]
@@ -84,39 +98,55 @@ def main():
         # display the image
 
         else:
+            #rospy.loginfo("------------DEBUG SIGNAL-------------------")
 #            print "STICKY NOTE DETECTED!"      #for debugging
             sticky = 1
+
 ###-------blob detection algorithm from
 ###-------https://www.learnopencv.com/find-center-of-blob-centroid-using-opencv-cpp-python/
-#            cv2.imshow("Image", img)
-#            cv2.waitKey(0)
+
             # convert the image to grayscale
             gray_image = cv2.cvtColor(res, cv2.COLOR_RGB2GRAY)
-
+            #cv2.imshow("Image", gray_image)
+            #cv2.waitKey(0.1)
             # convert the grayscale image to binary image
-            ret,thresh = cv2.threshold(gray_image,127,255,0)
+            ret,thresh = cv2.threshold(gray_image,127,255,cv2.THRESH_BINARY)
 
             # find contours in the binary image
-            contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+            contours, hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+            offx = 0
+            offy = 0
+            height, width = img.shape[:2]
+
             for c in contours:
                 # calculate moments for each contour
                 M = cv2.moments(c)
-
+                #rospy.loginfo("m10: %s m01: %s m00: %s mdiv:",M["m10"],M["m01"],M["m00"])
+                #rospy.loginfo("mdiv: %s",M["m10"]/M["m00"])
                 # calculate x,y coordinate of center
-                cX = int(M["m10"] / M["m00"])
-                cY = int(M["m01"] / M["m00"])
+                if(M["m00"] != 0):
+                    cX = int(M["m10"] / M["m00"])
+                    cY = int(M["m01"] / M["m00"])
+                else:
+                    cX = int(width/2)
+                    cY = int(height/2)
                 cv2.circle(img, (cX, cY), 5, (0, 0, 255), -1)
-                height, width = img.shape[:2]
-                offx = int(width/2-cX)
+                offx = int(cX-width/2)
                 offy = int(height/2-cY)
+                break
 #                rospy.loginfo("STICKY DETECTED!!!")
             if ir > 60:
                 ir = 0.4
     #               rospy.loginfo("x= %s y= %s width= %s height= %s", cX, cY, width, height)
     #               rospy.loginfo("x-offset= %s  y-offset= %s", offx, offy)
-                # display the image
-#               cv2.imshow("Image", img)
-#               cv2.waitKey(0)
+
+####UNCOMMENT FOR IMAGE DISPLAY TROUBLESHOOTING
+#        cv2.imshow("Image", mask)
+#        cv2.waitKey(20)
+#        rospy.sleep(1)
+#        cv2.destroyAllWindows()
+
         arr = CamArray()
         arr.visible = sticky
         arr.xoffset = float(offx)/float(width/2)
@@ -134,9 +164,6 @@ def main():
         cam_pub.publish(arr)
         rate = rospy.Rate(10) # 10hz
         rate.sleep()
-	#cam = baxter_interface.camera.CameraController("right_hand_camera")
-	#cam.open()
-
 
 
 if __name__ == '__main__':
