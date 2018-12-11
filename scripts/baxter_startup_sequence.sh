6681c5d113352565b04db7b218b17f6507dee05d
haydenm2 <haydenmmorgan@gmail.com>
2018-12-10 20:42:15 -0700

    First revision everything is working! Small tuning needed to make it smoother still and can get caught in singularities, but it is functional.

--- scripts/baxter_startup_sequence.sh
+++ scripts/baxter_startup_sequence.sh
@@ -2,7 +2,9 @@
 
 cd ~/baxter_ws   #moves you to the correct folder
 ./baxter.sh           #sets up environment variables correct for baxter to work
+sleep 1s
 rosrun baxter_tools enable_robot.py -r   #this resets the robot state if you pushed the e-stop
+sleep 1s
 rosrun baxter_tools enable_robot.py -e   #this re-enables the robot, and it may make a noise
+sleep 1s
 rosrun baxter_tools tuck_arms.py -u        #this will untuck his arms
-ipython
--- scripts/controller.py
+++ scripts/controller.py
@@ -6,7 +6,7 @@ import numpy as np
 #from copy import deepcopy #for copying numpy arrays as seperate variables
 #from threading import RLock, Timer
 #import time
-from math import pi
+import math
 from baxter_interface.limb import Limb
 from rad_baxter_limb.rad_baxter_limb import RadBaxterLimb
 from baxter_pykdl import baxter_kinematics as b_kin
@@ -47,45 +47,61 @@ def camera_callback(message):
     rospy.loginfo('%s %s %s %s',xOffset, yOffset, distance, visible)
 
 def main():
+    rospy.sleep(0.5)
+    
     global visible
     global xOffset
     global yOffset
     global distance
 
-    xOffset = .5
-    yOffset = -.5
-    errorMag = sqrt(xOffset^2 + yOffset^2)
+    visible = 0
+    xOffset = 0
+    yOffset = 0
+    distance = 0.2
+
+    # rospy.loginfo('%s', visible)
+
+    # xOffset = -.5
+    # yOffset = -.5
 
     # rospy.sleep(1)
     camsub = rospy.Subscriber('/cam_data', CamArray, camera_callback, queue_size=1)
-    c = .2
+    kd = 0.005
+    kx = 0.02
+    ky = 0.01
+    d = 0.2
+
 
     while not rospy.is_shutdown():
+    	errorMag = math.sqrt(xOffset*xOffset + yOffset*yOffset)
+    	errorD = d-distance
+        rospy.loginfo('%s',errorMag)
 	    # if visible?
-	    # get fk
-        fk = limb.get_kdl_forward_position_kinematics()
-        # rospy.loginfo("%s",fk)
-	    # scale camera data to position and find error
-        error = [xOffset*c,yOffset*c]
-	    # get angles
-        q = limb.get_joint_angles()
-        s = "***Actual q***"
-        rospy.loginfo('%s',s)
-        rospy.loginfo('%s',q)
-	    # get pseudo inverse of J
-        Jinv = limb.get_kdl_jacobian_pseudo_inverse()
-        # rospy.loginfo("%s",Jinv)
-        twist = np.transpose([[error[0], error[1], 0, 0, 0, 0]])
-        # rospy.loginfo('%s',twist)
-        q = q + np.transpose(Jinv*twist)
-        q = np.squeeze(np.asarray(q))
-        s2 = "***Projected q***"
-        rospy.loginfo('%s',s2)
-        rospy.loginfo('%s',q)
-	    # command baxter to move
-        # q = [0.09,-1,1.16,1.94,-.66,1.02,0]
-        control_rate = rospy.Rate(500)
-        limb.set_joint_positions_mod(q)
+        if visible and (errorMag > 0.1 or errorD > 0.01):
+		    # get fk
+	        fk = limb.get_kdl_forward_position_kinematics()
+	        # rospy.loginfo("%s",fk)
+		    # scale camera data to position and find error
+	        error = [xOffset*-kx,yOffset*ky,errorD*kd]
+		    # get angles
+	        q = limb.get_joint_angles()
+	        # s = "***Actual q***"
+	        # rospy.loginfo('%s',s)
+	        # rospy.loginfo('%s',q)
+		    # get pseudo inverse of J
+	        Jinv = limb.get_kdl_jacobian_pseudo_inverse()
+	        # rospy.loginfo("%s",Jinv)
+	        twist = np.transpose([[error[1], error[0], errorD, 0, 0, 0]])
+	        # rospy.loginfo('%s',twist)
+	        q = q + np.transpose(Jinv*twist)
+	        q = np.squeeze(np.asarray(q))
+	        # s2 = "***Projected q***"
+	        # rospy.loginfo('%s',s2)
+	        # rospy.loginfo('%s',q)
+		    # command baxter to move
+	        # q = [0.784,-0.740,0.144,1.328,-0.174,0.988,0.021]
+	        limb.set_joint_positions_mod(q)
+        control_rate = rospy.Rate(10)
         control_rate.sleep()
 
 if __name__ == '__main__':
